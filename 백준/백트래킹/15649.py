d6401d0303077acfd6d4bd4e3eeb079cb46ac2cf
chominho14 <chominho14@naver.com>
2022-07-12 15:34:45 +0900

    22.07.12 - 2

--- 백준/백트래킹/15649.py
+++ 백준/백트래킹/15649.py
@@ -3,21 +3,21 @@ N, M = map(int, input().split())
 # 스택을 채울 리스트
 s = []
 
-def dfs():
+def dfs(start):
     # 값을 찾았으면 출력
     if len(s) == M:
         print(' '.join(map(str, s)))
         return
     
-    for i in range(1, N + 1):
+    for i in range(start, N + 1):
         # 값이 중복이라면 배제
         if i in s:
             continue
         # 해당 경우의 수 추가
         s.append(i)
         # 깊이 탐색 동작
-        dfs()
+        dfs(i)
         # 이전 상황 돌아가기
         s.pop()
         
-dfs()
+dfs(1)
