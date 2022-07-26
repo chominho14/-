31a8210ac70f251fe147aa43bd4a9f0cc2d9641b
chominho14 <chominho14@naver.com>
2022-07-26 21:26:28 +0900

    22.07.26 - 1

---
+++ 백준/스택/4949.py
@@ -0,0 +1,28 @@
+
+
+while True:
+    a = input()
+    s = []
+    
+    if a == ".":
+        break
+    
+    for i in a:
+        if i == '[' or i == '(':
+            s.append(i)
+        elif i == ']':
+            if len(s) != 0 and s[-1] == '[':
+                s.pop()
+            else:
+                s.append(']')
+                break
+        elif i == ')':
+            if len(s) != 0 and s[-1] == '(':
+                s.pop()
+            else:
+                s.append(')')
+                break
+    if len(s) == 0:
+        print("yes")
+    else:
+        print("no")
