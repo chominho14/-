fe002292b1ec1f4f8a6a37ac28cce60481af92d4
chominho14 <chominho14@naver.com>
2022-08-17 17:15:20 +0900

    22.08.17 - 3

---
+++ 백준/동적계획법/9461.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+dp = [0] * 101
+
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+dp[4] = 2
+dp[5] = 2
+for _ in range(t):
+    n = int(input())
+    for i in range(6, n+1):
+        dp[i] = dp[i-1] + dp[i-5]
+        
+    print(dp[n])
