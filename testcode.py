faa40fe8dd6ed2f64ccfd85aa8b980712cd5fec3
chominho14 <chominho14@naver.com>
2023-01-15 22:07:15 +0900

    23.01.15 - 1

--- testcode.py
+++ testcode.py
@@ -1,5 +1,45 @@
 
 
+def solution(n, quests):
+    graph = [[] for _ in range(n+1)]
+    for i in range(len(quests)):
+        u, v = quests[i]
+        graph[v].append(u)
+        
+
+    print(graph)
+    visited = [False] * (n+1)
+    cnt = 1
+    result=[]
+    re_num = []
+    for i in range(1,n+1):
+        if len(re_num)==0:
+            print(visited)
+            if not visited[i]:
+                if len(graph[i]) == 0:
+                    result.append(i)
+                    visited[i] = True
+                else:
+                    for j in range(len(graph[i])):
+                        if not visited[j]:
+                            re_num.append(i)
+                            break
+        else:
+            for k in re_num:
+                check_p=True
+                for l in range(len(graph[k])):
+                    if not visited[l]:
+                        check_p=False
+                if check_p:
+                    visited[k]=True
+    print(result)
+                    
+
+
+
+
+
+
 # s = "111333344"
 # r = []
 # new_r = []
